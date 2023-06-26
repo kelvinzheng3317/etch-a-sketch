7931384e5e5416a84cee66eea9fede1060ad6f77
kelvinzheng3317 <kelvinzheng21@gmail.com>
2023-06-25 19:23:07 -0500

    set up grid

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,1 +1,23 @@
-
+let grid = document.querySelector('.grid');
+let unit;
+let row;
+let selectedColor = 'rgb(0, 0, 0)';
+for (let i=0; i<16; ++i) {
+    row = document.createElement('div');
+    row.classList.add('row');
+    for (let j=0; j<16; ++j) {
+        unit = document.createElement('div');
+        unit.classList.add('unit');
+        unit.setAttribute('id',`unit${16*i + j}`);
+        unit.addEventListener('mousedown', () => {
+            document.querySelector(`#unit${16*i+j}`)
+            .style.backgroundColor =  selectedColor;
+        })
+        unit.addEventListener('mouseover', () => {
+            document.querySelector(`#unit${16*i+j}`)
+            .style.backgroundColor =  selectedColor;
+        })
+        row.appendChild(unit);
+    }
+    grid.appendChild(row);
+}

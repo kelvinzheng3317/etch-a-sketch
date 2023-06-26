8f39cd431e1a05985e671c36121ac1edcda0b516
kelvinzheng3317 <kelvinzheng21@gmail.com>
2023-06-26 13:25:14 -0500

    added multiple color options and clear button

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,17 +1,42 @@
 const grid = document.querySelector('.grid');
 let unit;
 let row;
+
+let colorType = 'single';
 let selectedColor = 'rgb(0, 0, 0)';
 let mousedown = false;
 
 document.body.onmousedown = () => { mousedown = true; }
 document.body.onmouseup = () => { mousedown = false; }
 
+/* adds event listener for color select buttons */
+document.querySelector('.selectBlack').addEventListener('click', 
+    () => { colorType = 'single'; selectedColor = 'rgb(0,0,0)'; })
+document.querySelector('.selectRandom').addEventListener('click', 
+    () => { colorType = 'random'; })
+document.querySelector('.selectEraser').addEventListener('click', 
+    () => { colorType = 'single'; selectedColor = 'rgb(255,255,255)'; })
+document.querySelector('.clear').addEventListener('click',
+    () => clearAll())
+
+
 function updateColor(e) {
     if (e.type === "mouseover" && !mousedown) {
         return;
     }
-    e.target.style.backgroundColor = selectedColor;
+    if (colorType === 'single') {
+        e.target.style.backgroundColor = selectedColor;
+    } else if (colorType === 'random') {
+        let randRed = Math.random() * 255;
+        let randGreen = Math.random() * 255;
+        let randBlue = Math.random() * 255;
+        e.target.style.backgroundColor = `rgb(${randRed}, ${randGreen}, ${randBlue})`;
+    }
+}
+
+function clearAll() {
+    document.querySelectorAll('.unit').forEach(
+        unit => { unit.style.backgroundColor = 'white'; });
 }
 
 for (let i=0; i<16; ++i) {
